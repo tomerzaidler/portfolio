a4fc5c75082f61919594e52f6c1de7a0c94a74e6
tomerzaidler <tomerzaidler@gmail.com>
2020-03-21 18:07:24 +0200

    working on about page

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -12,25 +12,18 @@ class About extends React.Component {
                 <div className="ui divider" style={{ marginBottom: 40 }}></div>
                 <div className="abouttext">
                     <div className="ui center aligned container">
-                        <p style={{ color: 'white', fontSize: 25 }}>
-                            Born in Israel and raised in Rishon Le Zion. I love to cook for my family and am a big
-                            Hip-Hop fan. Since childhood I have loved to deal with computers, from Elementary
-                            School until Today.
+                        <p className="info">
+                            Born in Israel and raised in Rishon Le Zion. My career began in IBM-Trusteer as a NOC
+                            Engineer, Today my role is a full-stack developer at ArtCode Labs. Since childhood I
+                            love to deal with computers, from Elementary School until Today.
                         </p>
-                        <div style={{ color: 'white', fontSize: 25, marginBottom: 10 }}>
-                            I'm a student with outstanding self-learning skills,highly motivated, responsible and
-                            reliable.
-                        </div>
-                        <div style={{ color: 'white', fontSize: 25, marginBottom: 10 }}>
-                            I'm a TEAMPLAYER, BORN LEADER, AFFABLE. Tet high school.
-                        </div>
-                        <div style={{ color: 'white', fontSize: 25, marginBottom: 10 }}>
-                            {' '}
-                            Study - 2nd year Computer Science B.Sc Student at H.I.T.
-                        </div>
-                        <div style={{ color: 'white', fontSize: 25 }}>
-                            Finished Computer Science profession at high school.
-                        </div>
+                        <p className="info">
+                            * I'm a student with outstanding self-learning skills,highly motivated, responsible and
+                            reliable *
+                        </p>
+                        <div className="info">* I'm a TEAMPLAYER, BORN LEADER, AFFABLE. Tet high school *</div>
+                        <div className="info">* Study - 2nd year Computer Science B.Sc Student at H.I.T *</div>
+                        <div className="info">* Finished Computer Science profession at high school *</div>
                     </div>
                 </div>
             </div>
