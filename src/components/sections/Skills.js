ef7fd5245e1e323b8eb816c7c41688ac3e5eae91
tomerzaidler <tomerzaidler@gmail.com>
2020-03-22 11:41:46 +0200

    bulding skills page

--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -1,10 +1,63 @@
 import React from 'react';
+import TextEffect from '../TextEffect';
+import './skills.css';
 
 class Skills extends React.Component {
     render() {
         return (
             <div className="ui container">
-                <div className="ui button red">Skills</div>
+                <div style={{ marginBottom: 100, textAlign: 'center' }}>
+                    <TextEffect font="Barrio" size="4" delay="300" text="Skills" />
+                </div>
+                {/* <div style={{ marginBottom: 100 }}></div> */}
+                <div class="ui divided four column grid">
+                    <div class="row">
+                        <div class="column">
+                            <div className="icon">
+                                <i class="devicon-nodejs-plain-wordmark "></i>
+                            </div>
+                            <div className="icon">
+                                <i class="devicon-c-plain "></i>
+                            </div>
+                            <div className="icon">
+                                <i class="devicon-mongodb-plain-wordmark"></i>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div className="icon">
+                                <i class="devicon-react-original-wordmark"></i>
+                            </div>
+                            <div className="icon">
+                                <i class="devicon-cplusplus-plain "></i>
+                            </div>
+                            <div className="icon">
+                                <i class="devicon-amazonwebservices-plain-wordmark "></i>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div className="icon">
+                                <i class="devicon-javascript-plain "></i>
+                            </div>
+                            <div className="icon">
+                                <i class="devicon-java-plain-wordmark "></i>
+                            </div>
+                            <div className="icon">
+                                <i class="devicon-postgresql-plain-wordmark"></i>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div className="icon">
+                                <i class="devicon-python-plain-wordmark "></i>
+                            </div>
+                            <div className="icon">
+                                <i class="devicon-django-plain "></i>
+                            </div>
+                            <div className="icon">
+                                <i class="devicon-ubuntu-plain-wordmark "></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
